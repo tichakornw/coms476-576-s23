cb74d52fc3f7034cd7c92f4e4e3088bc4b5cc77c
Ibne Farabi Shihab <ibnfarabishihab@gmail.com>
2023-02-09 01:39:06 -0600

    Revert "finished HW without report"
    This reverts commit d4526168a7721e76c4c9a794425c622940321495.

--- discrete_search.py
+++ discrete_search.py
@@ -1,17 +1,13 @@
 ALG_BFS = "bfs"
 ALG_DFS = "dfs"
 ALG_ASTAR = "astar"
-from queue import Queue
-
 
 
 class StateSpace:
     """A base class to specify a state space X"""
-    
+
     def __contains__(self, x):
         """Return whether the given state x is in the state space"""
-        
-
         raise NotImplementedError
 
     def get_distance_lower_bound(self, x1, x2):
@@ -53,99 +49,4 @@ def fsearch(X, U, f, xI, XG, alg):
                states visited during the search and path is a path from xI to a state in XG
     """
     # TODO: Implement this function
-
-    
-
-    queue = get_queue(alg, X, XG)
-    visited = set()
-    queue.insert(xI, None)
-
-    while queue.queue:
-        x = queue.pop()
-        print("start",x)
-        if x in visited:
-            continue
-        visited.add(x)
-        
-        if x in XG:
-            path = []
-            while x is not None:
-                path.insert(0, x)
-                x = queue.parent[x]
-            return {'visited': list(visited), 'path': path}
-        for u in U(x):
-            print("the actions",u)
-            x_new = u
-            print("new actions",x_new)
-            
-            if x_new not in visited:
-                queue.insert(x_new, x)
-
-    return {'visited': list(visited), 'path': []}
-
     raise NotImplementedError
-
-class Queue:
-    def __init__(self):
-        self.queue = []
-        self.parent = {}
-
-    def insert(self, x, parent):
-        pass
-
-    def pop(self):
-        pass
-
-
-class QueueBFS(Queue):
-    def insert(self, x, parent):
-        self.queue.append(x)
-        self.parent[x] = parent
-
-    def pop(self):
-        return self.queue.pop(0)
-
-
-class QueueDFS(Queue):
-    def insert(self, x, parent):
-        self.queue.insert(0, x)
-        self.parent[x] = parent
-
-    def pop(self):
-        return self.queue.pop(0)
-
-
-class QueueAstar(Queue):
-    def __init__(self, X,XI XG):
-        self.X = X
-        self.XG = XG
-        self.queue = []
-        self.parent = {}
-        self.cost = {}
-        self.cost[XI]=0
-
-    def insert(self, x, parent):
-        self.queue.append(x)
-        self.parent[x] = parent
-        if parent is not None:
-            self.cost[x] = self.cost[parent] + 1
-        else:
-            self.cost[x] = 0
-        for goal in self.XG:
-            self.cost[x] += self.X.get_distance_lower_bound(x, goal)
-
-    def pop(self):
-        index = self.queue.index(min(self.queue, key=lambda x: self.cost[x]))
-        x = self.queue.pop(index)
-        return x
-
-
-def get_queue(alg, X,XI, XG):
-    if alg == 'bfs':
-        return QueueBFS()
-    elif alg == 'dfs':
-        return QueueDFS()
-    elif alg == 'astar':
-        return QueueAstar(X,XI, XG)
-
-    
